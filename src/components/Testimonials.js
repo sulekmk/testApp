383272e056da739c95d947effe94db9be081059c
sulekk-arrcus <sulek.k@arrcus.com>
2023-11-22 13:30:28 -0800

    commit

---
+++ src/components/Testimonials.js
@@ -0,0 +1,42 @@
+
+import React from "react";
+import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
+import { testimonials } from "../data";
+
+export default function Testimonials() {
+  return (
+    <section id="testimonials">
+      <div className="container px-5 py-10 mx-auto text-center">
+        <UsersIcon className="w-10 inline-block mb-4" />
+        <h1 className="sm:text-4xl text-3xl font-medium title-font text-black mb-12">
+          Client Testimonials
+        </h1>
+        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+          {testimonials.map((testimonial) => (
+            <div className="p-4 md:w-1/2 w-full">
+              <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
+                <TerminalIcon className="block w-8 text-gray-500 mb-4" />
+                <p className="leading-relaxed mb-6">{testimonial.quote}</p>
+                <div className="inline-flex items-center">
+                  {/* <img
+                    alt="testimonial"
+                    src={testimonial.image}
+                    className="w-12 rounded-full flex-shrink-0 object-cover object-center"
+                  /> */}
+                  <span className="flex-grow flex flex-col pl-4">
+                    <span className="title-font font-medium text-white">
+                      {testimonial.name}
+                    </span>
+                    <span className="text-black-500 text-sm uppercase">
+                      {testimonial.company}
+                    </span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "DAVID CHU'S CHINA BISTRO Restaurant",
+    subtitle: "Restaurant Web Application",
+    description:
+      "In this assignment, The front page of our web app contains 3 clickable tiles: Menu, Specials, and Map. If you click on the Specials tile, you will be taken to a single category page where all the menu items that belong to the Specials menu category will be shown. Your task in this assignment is to alter this behavior such that when the user clicks on the Specials tile, the web app takes the user to a random single category menu page, listing menu items in the category, be it 'Lunch', 'Dinner', 'Sushi', etc. That way, any random category can become the Specials!",
+    image: "./project-1.png",
+    link: "https://sulekmk.github.io/github.io-module5-solution/",
+  },
+  {
+    title: "CICD System",
+    subtitle: "ARRCUS - CICD SYSTEM",
+    description:
+      "The CICD UI can be used for managing assets in an organization along with the users and groups of that organization. A user of this application can add multiple different types of assets and give or take away access to those assets at the group level. Examples of this as well as other use cases will be provided in this document. I have worked on topolody management module",
+    image: "./project-5.png",
+    link: "https://cicd.sjc.arrcus.com",
+  },
+  {
+    title: "Discover-Ecommerce",
+    subtitle: "Discover-Ecommerce: Login and Payments Domain",
+    description:
+      "The Discover-Ecommerce application is a mobile custom application developed for ANDROID/iPhone/iPad devices. JQM: Discover business would like mobile applications to have rich UI experience similar to native clients. The main purpose of JQM Application Development is to build a hybrid cross platform mobile application and rewrite the current mobile website of Discover with enhanced features. Research and development using jQuery mobile API, CSS3, HTML5, jQuery technologies.",
+    image: "./project-2.gif",
+    link: "https://www.discover.com/online-banking/savings-account/",
+  },
+  {
+    title: "European Weather Forecast - API Project",
+    subtitle: "Coursera- WeatherAPIProject",
+    description:
+      "The challenge will be to write a webpage that uses API to retrieve weather information from an external source. To do this, you will create the user interface, send the request to the external source using JavaScript, process data received and present the information on your webpage using HTML.In this project, we'll use 7Timer API, which is free of charge and does not require any API keys. Approach : HTML5 webpage created, Bootstrap CSS and external stylesheet used, 7Timer API used to retrieve weather information on selected city and country. Code: Formatting, Commenting used to maintain quality of code. avoided redundancy of code.",
+    image: "./project-6.png",
+    link: "https://sulekmk.github.io/WeatherAPIProject/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Good learning ability and contributing to the CICD Application",
+    name: "Amaresan Balakrishnan",
+    company: "Arrcus Inc. San Jose, California, USA",
+  },
+  {
+    quote:
+      "Sulek has been responsive and quick to learn the complex situation and with a quick turnaround provide the required solution in place. Works within the constraints in the account team with great flexibility with the end result in mind. 1.Possesses outstanding technical skills and is extremely flexible in meeting project requirements.2.Spearheaded Architectural design for a new complex development project which was appreciated by Client Technology team also. 3.Maintained thorough supervision of the project design and development to ensure high-class deliverable, Project went through UAT without any major defect much to the surprise of business team. 4.Apart from ensuring high quality deliverable, made contributions to proposals by designing the high-level technical Solution/Architecture and also extended support for technical estimation. 5.Helped account team through mentoring of junior team members on the technology advancements. 6.Communicates effectively and is confident. Her dedication and sense of ownership is excellent!",
+    name: "Rahul Borade",
+    company: "Cognizant Technology Solutions Pune, India",
+  },
+];
+
+export const skills = [
+  "React JS",
+  "HTML5",
+  "CSS3",
+  "JavaScript",
+  "Material UI",
+  "SQL Queries",
+  "MongoDB UI Data Retrieval",
+  "GitHub",
+  "Jira",
+  "Visual Studio Code",
+  "Manual Testing - UI Modules",
+  "React Table",
+  "Single Column - Sorting and Filtering",
+  "Multiple Column - Sorting and Filtering",
+  
+];
